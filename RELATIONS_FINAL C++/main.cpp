57fa39f6ed2c2040bc07bd059d8a058d0958f84d
Alex Chen <alexchen@Alexs-MacBook-Air.local>
2014-06-21 10:51:53 +0800

    Initial Commit

---
+++ RELATIONS_FINAL C++/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RELATIONS_FINAL C++
+//
+//  Created by Alex Chen on 2014-06-21.
+//  Copyright (c) 2014 Alex Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
